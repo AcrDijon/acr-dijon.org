28717fca9463a714e0b7ddaacf2c09a6b2652b00
Tarek Ziade <tarek@ziade.org>
2015-05-08 13:30:50 +0200

    using my own domain for now

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://acr-dijon.org'
+SITEURL = 'http://acr.ziade.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
